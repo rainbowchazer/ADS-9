10d6e0cfe130290d56175373b9a85763ecd35d6e
rainbowchazer <125148013+rainbowchazer@users.noreply.github.com>
2023-06-04 13:27:22 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -2,4 +2,59 @@
 #ifndef INCLUDE_BST_H_
 #define INCLUDE_BST_H_
 
+template < typename T >
+class BST
+{
+private:
+	struct Node
+	{
+		T word;
+		int count;
+		Node* left;
+		Node* right;
+	};
+	Node* root;
+	int search(Node* root, T word) {
+		if (root == nullptr) return 0;
+		else if (root->word == word)
+			return root->count;
+		else if (word < root->word) return search(root->left, word);
+		else return search(root->right, word);
+	}
+	Node* addNode(Node* root, T word) {
+		if (root == nullptr) {
+			root = new Node;
+			root− > word = word;
+			root− > count = 1;
+			root− > left = root− > right = nullptr;
+		}
+		else if (root− > word > word) {
+			root− > left = addNode(root− > left, word);
+		}
+		else if (root− > word < word) {
+			root− > right = addNode(root− > right, word);
+		}
+		else
+			root− > count++;
+		return root;
+	}
+	int depth(Node* root) {
+		if (root == nullptr) return 0;
+		else
+			return std::max(getDepth(root->left), getDepth(root->right)) + 1;
+	}
+public:
+	BST() : root(nullptr) {}
+	void addword(T word) {
+		root = addNode(root, word);
+	}
+	int Depth() {
+		return depth(root) - 1;
+	}
+	int Search(T word) {
+		Node* temp = searchNode(root, word);
+		return (temp) ? node->count : 0;
+	}
+};
+
 #endif  // INCLUDE_BST_H_
